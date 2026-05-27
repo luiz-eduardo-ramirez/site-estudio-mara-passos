4660e0a463316071700d7536e228dde2f2e6e322
Luiz Eduardo Ramirez <luizeduramirez@gmail.com>
2026-05-27 09:51:30 -0300

    Fix: metadata

--- mara-passos-website/src/app/sobre-nos/page.tsx
+++ mara-passos-website/src/app/sobre-nos/page.tsx
@@ -4,7 +4,7 @@ import Footer from '../../components/layout/Footer';
 import AboutUs from '../../components/sections/AboutUs';
 
 export const metadata: Metadata = {
-  title: 'Quem Somos | Estúdio Mara Passos - Escola de Música em São Paulo',
+  title: 'Quem Somos | Estúdio Mara Passos - Escola de Música na Lapa, São Paulo',
   description: 'Descubra a história e a missão do Estúdio Mara Passos, uma escola de música com metodologia lúdica, curativa e acolhedora em São Paulo.',
 };
 
